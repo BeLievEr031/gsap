d3d61b3deec29649ccce0992842953ba092ab965
BeLieVer031 <sandyrajak031@gmail.com>
2024-02-28 18:24:23 +0530

    section-2 and section-3 half completed

--- trionn/learning/script.js
+++ trionn/learning/script.js
@@ -1,7 +1,6 @@
 const customCursor = document.querySelector(".custom-cursor")
 
 document.addEventListener("mousemove", (e) => {
-    console.log(e.x,e.y);
     customCursor.style.transform = `translate(${e.x - 10}px,${e.y - 10}px)`
 })
 
@@ -11,12 +10,39 @@ gsap.registerPlugin(ScrollTrigger)
 gsap.to(".video-cont", {
     scrollTrigger: {
         trigger: ".video-cont",
-        // markers: true,
-        start: "top 65%",
-        end:"top 40%",
+        start: "top 75%",
+        end: "top 40%",
         scrub: 1
     },
-    top:"180%",
+    top: "165%",
     width: "95vw",
-    height:"120vh",
+    height: "100vh",
+    borderRadius:"25px"
 })
+
+
+const tl1 = gsap.timeline(
+    {
+        scrollTrigger: {
+            trigger: ".sec-3-h-1",
+            start: "top 95%",
+            end: "top 15%",
+            scrub: 1,
+        }
+    }
+)
+
+tl1.from(".sec-3-h-1", {
+    x: "-20vw"
+}, "sec2-h-animate")
+
+tl1.to(".span-2",{
+    width:"100%"
+},"sec2-h-animate")
+
+
+tl1.from(".sec-3-h-2", {
+    x: document.querySelector(".sec-3-h-1").clientWidth-100,
+}, "sec2-h-animate")
+
+
