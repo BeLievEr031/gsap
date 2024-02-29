5944608173b98d42d0e6b6a6a70889df9334553a
BeLieVer031 <sandyrajak031@gmail.com>
2024-02-29 18:05:06 +0530

    logo init done

--- trionn/learning/script.js
+++ trionn/learning/script.js
@@ -7,17 +7,20 @@ document.addEventListener("mousemove", (e) => {
 
 gsap.registerPlugin(ScrollTrigger)
 
-gsap.to(".video-cont", {
+const tl = gsap.timeline({
     scrollTrigger: {
         trigger: ".video-cont",
         start: "top 75%",
-        end: "top 40%",
-        scrub: 1
-    },
+        end: "top 10%",
+        scrub: 1,
+    }
+})
+
+tl.to(".video-cont", {
     top: "165%",
     width: "95vw",
     height: "100vh",
-    borderRadius:"25px"
+    borderRadius: "25px",
 })
 
 
@@ -28,6 +31,7 @@ const tl1 = gsap.timeline(
             start: "top 95%",
             end: "top 15%",
             scrub: 1,
+            // markers: true
         }
     }
 )
@@ -36,13 +40,44 @@ tl1.from(".sec-3-h-1", {
     x: "-20vw"
 }, "sec2-h-animate")
 
-tl1.to(".span-2",{
-    width:"100%"
-},"sec2-h-animate")
-
-
-tl1.from(".sec-3-h-2", {
-    x: document.querySelector(".sec-3-h-1").clientWidth-100,
+tl1.to(".span-2", {
+    width: "100%"
 }, "sec2-h-animate")
 
 
+// gsap.to(, "sec2-h-animate")
+
+
+tl1.from(".sec-3-h-2", {
+    x: document.querySelector(".sec-3-h-1").clientWidth - 400,
+}, "sec2-h-animate")
+
+
+const tl2 = gsap.timeline(
+    {
+        scrollTrigger: {
+            trigger: ".sec-3-content",
+            start: "top 90%",
+            end: "top 65%",
+            scrub: 1,
+            // markers:true
+        }
+    }
+)
+
+tl2.from(".sec-3-text span", {
+    y: "100%",
+    stagger: .5
+})
+
+gsap.to(".tiger-logo", {
+    right: "20px",
+    opacity:1,
+    scrollTrigger: {
+        trigger: ".logo-init",
+        // markers: true,
+        start:"top 90%",
+        end:"top 70%",
+        scrub:1
+    }
+})
